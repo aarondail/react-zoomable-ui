3209b76d522c92b557b2e9e16322c0b62ee41c49
Aaron Dail <aaron@planetdail.com>
2020-01-30 21:52:34 -0800

    More scroll supression in suppressBrowserScrollingAndZooming.

--- src/suppressBrowserScrollingAndZooming.ts
+++ src/suppressBrowserScrollingAndZooming.ts
@@ -1,26 +1,36 @@
-export type CancelSuppressZoomingCallback = () => void;
+export type CancelSuppressBrowserScrollingAndZoomingCallback = () => void;
 
 /**
  * This is not used by the ViewPort, but if you have HTML elements around the
  * ViewPort you may want to suppress zooming or panning on the whole page. This
  * function will do that.
  */
-export function suppressBrowserScrollingAndZooming(element: HTMLElement): CancelSuppressZoomingCallback {
+export function suppressBrowserScrollingAndZooming(): CancelSuppressBrowserScrollingAndZoomingCallback {
   const handleWheel = (e: WheelEvent) => {
     // If we want to just suppress pinch-gestures on desktop browsers we can
     // look for whether the control key is pressed in the event. For whatever
     // reason, that can distinguish scrolling and pinching in this event.
     e.preventDefault();
   };
+  const handleGestureStartForSafari = (e: any) => {
+    e.preventDefault();
+  };
   const handleGestureChangeForSafari = (e: any) => {
     e.preventDefault();
   };
+  const handleTouchMove = (e: any) => {
+    e.preventDefault();
+  };
 
-  element.addEventListener('wheel', handleWheel);
-  element.addEventListener('gesturechange', handleGestureChangeForSafari);
+  document.addEventListener('wheel', handleWheel);
+  document.addEventListener('gesturestart', handleGestureStartForSafari);
+  document.addEventListener('gesturechange', handleGestureChangeForSafari);
+  document.addEventListener('touchmove', handleTouchMove, { passive: false });
 
   return () => {
-    element.removeEventListener('wheel', handleWheel);
-    element.removeEventListener('gesturechange', handleGestureChangeForSafari);
+    document.removeEventListener('wheel', handleWheel);
+    document.removeEventListener('gesturestart', handleGestureStartForSafari);
+    document.removeEventListener('gesturechange', handleGestureChangeForSafari);
+    document.removeEventListener('touchmove', handleTouchMove);
   };
 }
